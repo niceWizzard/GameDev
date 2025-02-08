4fb223d0128d67e94fa4447f336db144ada98867
RIchard Manansala <richard232manansala@gmail.com>
2025-02-08 20:26:14 +0800

    Added limited ammo capacity & reloading times for gun

--- Assets/Weapons/Gun/GunController.cs
+++ Assets/Weapons/Gun/GunController.cs
@@ -7,8 +7,8 @@ using UnityEngine.Serialization;
 public class GunController : MonoBehaviour
 {
     [Header("Stats")] 
-    [SerializeField, Range(0, 15)] private int attackPerSecond = 2;
-
+    [SerializeField, Range(3, 15)] private int attackPerSecond = 4;
+    [SerializeField] private int ammoCapacity = 7;
     [SerializeField, Range(4, 10)] private int accuracy = 4;
     [Header("Components")]
     [SerializeField] private Transform leftNozzleTransform;
@@ -23,12 +23,15 @@ public class GunController : MonoBehaviour
     public GameObject Owner => owner;
     private Transform NozzleTransform => _spriteRenderer.flipY ? rightNozzleTransform : leftNozzleTransform;
 
+    private int _ammoCount;
     private bool _canShoot = true;
+    private bool _isReloading = false;
     private float AttackCd => 1f / attackPerSecond;
     private void Awake()
     {
         _camera = Camera.main;
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _ammoCount = ammoCapacity;
     }
 
     public void FlipSprite(bool isFlipped)
@@ -38,14 +41,22 @@ public class GunController : MonoBehaviour
 
     public void Shoot()
     {
-        if (!_camera || !_canShoot)
+        if (!_camera || !_canShoot || _isReloading)
             return;
         var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         _canShoot = false;
         var mouse = _camera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mouseDir = (mouse - Owner.transform.position).normalized;
         projectile.Setup(NozzleTransform.position,mouseDir, Owner ,accuracy);
-        StartCoroutine(StartAttackCdTimer());
+        StartCoroutine(--_ammoCount <= 0 ? StartReloadTimer() : StartAttackCdTimer());
+    }
+
+    private IEnumerator StartReloadTimer()
+    {
+        yield return new WaitForSeconds(2.5f);
+        _isReloading = false;
+        _ammoCount = ammoCapacity;
+        _canShoot = true;
     }
 
     private IEnumerator  StartAttackCdTimer()
