d34ffa578ac9d271c4bae659c94f06a68e5b4463
RIchard Manansala <richard232manansala@gmail.com>
2025-02-09 20:18:21 +0800

    Added: - MobDetector -> added player controller in GhostController

--- Assets/Mobs/Ghost/GhostController.cs
+++ Assets/Mobs/Ghost/GhostController.cs
@@ -1,3 +1,6 @@
+using System;
+using Lib;
+using Player;
 using UnityEngine;
 
 namespace Mobs.Ghost
@@ -7,9 +10,24 @@ namespace Mobs.Ghost
     {
         [SerializeField] private Rigidbody2D rigidbody2d;
         [SerializeField] private Collider2D collider2d;
+        [SerializeField] private MobDetector mobDetector;
     
         public Rigidbody2D Rigidbody2D => rigidbody2d;
         public Collider2D Collider2D => collider2d;
-    
+
+        [HideInInspector]
+        public PlayerController detectedPlayer;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            mobDetector.OnMobEntered += controller => detectedPlayer = controller as PlayerController;
+            mobDetector.OnMobExited += controller => detectedPlayer = null;
+        }
+
+        private void Update()
+        {
+            Debug.Log(detectedPlayer == null ? "NO PLAYER" : "PLAYER DETECED: " + detectedPlayer.name);
+        }
     }
 }
