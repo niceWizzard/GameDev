24a819c8a4aee085249f0f005cb45e3978bdb841
RIchard Manansala <richard232manansala@gmail.com>
2025-02-16 12:42:03 +0800

    Removed unused _hubPrefab

--- Assets/Main/Lib/Singleton/LevelLoader.cs
+++ Assets/Main/Lib/Singleton/LevelLoader.cs
@@ -1,24 +1,13 @@
 using System;
 using System.Collections;
-using System.Security.Cryptography;
-using Main.Player;
-using Main.UI;
-using Unity.VisualScripting;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Main.Lib.Singleton
 {
     public class LevelLoader : Singleton<LevelLoader>
     {
-        private HUDController _hudPrefab;
         public static event Action OnLevelChange;
-        public static void Setup(
-            HUDController hudPrefab) 
-        {
-            Instance._hudPrefab = hudPrefab;
-        }
 
         public void LoadLevel(SceneAsset levelAsset)
         {
