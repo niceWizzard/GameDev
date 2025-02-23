746e1bfba726770f036a5bda9f7d33082101b9a7
RIchard Manansala <richard232manansala@gmail.com>
2025-02-23 15:00:52 +0800

    Added a safeSpawn transform field for each Levelmanager

--- Assets/Main/Lib/Level/LevelManager.cs
+++ Assets/Main/Lib/Level/LevelManager.cs
@@ -3,13 +3,17 @@ using Main.Lib.Singleton;
 using Main.Player;
 using Main.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Main.Lib.Level
 {
     public class LevelManager : MonoBehaviour
     {
+        [SerializeField] private Transform safeSpawn;
         private void Awake()
         {
+            if(safeSpawn == null)
+                Debug.LogError($"Safe spawn is null at {SceneManager.GetActiveScene().name}");
             var player = FindAnyObjectByType<PlayerController>();
             MainCamera.Instance.Follow(player);
             HUDController.Instance.SetPlayer(player);
