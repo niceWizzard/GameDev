df88536df4c8ad08695ed2e401c44b3daa2d83e4
RIchard Manansala <richard232manansala@gmail.com>
2025-03-02 20:28:36 +0800

    Added option for LevelLoader to load from AssetReference

--- Assets/Main/Lib/Singleton/LevelLoader.cs
+++ Assets/Main/Lib/Singleton/LevelLoader.cs
@@ -28,17 +28,30 @@ namespace Main.Lib.Singleton
             blackScreen.color = color;
         }
 
+        public void LoadLevel(AssetReference levelRef)
+        {
+            StartCoroutine(LoadLevelCoroutine(levelRef));
+        }
         public void LoadLevel(string levelName)
         {
             StartCoroutine(LoadLevelCoroutine(levelName));
         }
-
-
+        
         private static IEnumerator LoadLevelCoroutine(string levelName)
         {
             Instance.blackScreen.DOFade(1, 0.25f).SetEase(Ease.InCubic);
             yield return new WaitForSeconds(0.25f);
             yield return Addressables.LoadSceneAsync(levelName).Yield();
+            yield return new WaitForSeconds(0.01f);
+            Instance.blackScreen.DOFade(0, 0.25f).SetEase(Ease.InCubic);
+        }
+
+
+        private static IEnumerator LoadLevelCoroutine(AssetReference levelRef)
+        {
+            Instance.blackScreen.DOFade(1, 0.25f).SetEase(Ease.InCubic);
+            yield return new WaitForSeconds(0.25f);
+            yield return levelRef.LoadSceneAsync().Yield();
             yield return new WaitForSeconds(0.01f);
             Instance.blackScreen.DOFade(0, 0.25f).SetEase(Ease.InCubic);
         }
