392cc0716cb5edc019f420884f6132d642ab1952
RIchard Manansala <richard232manansala@gmail.com>
2025-03-13 21:25:34 +0800

    -Removed UniqueId in ghost -removed unnecessary requirecomponent and CanAtttack prop

--- Assets/Main/World/Mobs/Ghost/GhostController.cs
+++ Assets/Main/World/Mobs/Ghost/GhostController.cs
@@ -1,30 +1,13 @@
-using CleverCrow.Fluid.UniqueIds;
 using Main.Lib.Mobs;
 using Main.Weapons;
 using UnityEngine;
 
 namespace Main.World.Mobs.Ghost
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     public class GhostController : EnemyController
     {
         [SerializeField] private ProjectileController projectilePrefab;
-
-
-        private UniqueId _uniqueId = null!;
-
         public ProjectileController ProjectilePrefab => projectilePrefab;
 
-        public bool CanAttack { get; set; } = true;
-
-
-        protected override void Awake()
-        {
-            base.Awake();
-            _uniqueId = GetComponent<UniqueId>();
-        }
-
-
-        
     }
 }
