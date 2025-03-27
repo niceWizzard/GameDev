18003c98f8f9dd9d747adedb9e9195ba4b011fc6
RIchard Manansala <richard232manansala@gmail.com>
2025-03-27 09:44:49 +0800

    Added states in DialogSystem to improve its logic

--- Assets/Main/Lib/Singleton/DialogSystem.cs
+++ Assets/Main/Lib/Singleton/DialogSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
@@ -17,7 +18,18 @@ namespace Main.Lib.Singleton
         [SerializeField] private TMP_Text senderText;
         [SerializeField] private TMP_Text continueText;
 
-        private bool _isShowing = false;
+        private enum DialogState
+        {
+            Closed,
+            Closing,
+            Opening,
+            DialogAnimation,
+            Completed,
+        }
+        
+        private DialogState _dialogState = DialogState.Closed;
+        
+        private string _currentDialog = "";
 
         protected override void Awake()
         {
@@ -32,7 +44,7 @@ namespace Main.Lib.Singleton
 
         public static void ShowDialog(string message, string sender="")
         {
-            if(Instance._isShowing)
+            if (Instance._dialogState != DialogState.Closed)
             {
                 Debug.LogWarning("DialogSystem is already showing");
                 return;
@@ -42,18 +54,24 @@ namespace Main.Lib.Singleton
 
         private void Update()
         {
-            if (!_isShowing)
-                return;
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (!Input.GetKeyDown(KeyCode.Space)) return;
+            switch (_dialogState)
             {
-                _ = CloseDialog();
+                case DialogState.DialogAnimation:
+                    _dialogState = DialogState.Completed;
+                    dialogText.text = _currentDialog;
+                    break;
+                case DialogState.Completed:
+                    _ = CloseDialog();
+                    break;
             }
         }
 
         private async UniTask CloseDialog()
         {
-            _isShowing = false;
+            _dialogState = DialogState.Closing;
             await dialogPanel.transform.DOScale(Vector3.zero, 0.5f).SetUpdate(true).SetLink(Instance.gameObject).AsyncWaitForCompletion();
+            _dialogState = DialogState.Closed;
             canvas.gameObject.SetActive(false);
             senderText.text = "";
             dialogText.text = "";
@@ -64,21 +82,25 @@ namespace Main.Lib.Singleton
         {
             Time.timeScale = 0;
             canvas.gameObject.SetActive(true);
+            continueText.gameObject.SetActive(false);
             dialogPanel.transform.localScale *= 0;
+            _dialogState = DialogState.Opening;
             await dialogPanel.transform.DOScale(Vector3.one, 0.5f).SetUpdate(true).SetLink(Instance.gameObject).AsyncWaitForCompletion();
-            _isShowing = true;
+            _dialogState = DialogState.DialogAnimation;
             senderText.text = sender;
+            _currentDialog = message;
             var t = "";
             foreach (var c in message)
             {
+                if (_dialogState != DialogState.DialogAnimation)
+                    break;
                 dialogText.text = t;
                 t += c;
                 await UniTask.WaitForSeconds(0.05f, ignoreTimeScale:true, cancellationToken:destroyCancellationToken);
             }
+            _dialogState = DialogState.Completed;
             continueText.gameObject.SetActive(true);
         }
 
-        
-        
     }
 }
