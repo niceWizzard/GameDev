c0e24b3a72a73560dc90897f0acaace29f604569
RIchard Manansala <richard232manansala@gmail.com>
2025-03-27 09:30:17 +0800

    Added time pausing and text animation in DialogSystem

--- Assets/Main/Lib/Singleton/DialogSystem.cs
+++ Assets/Main/Lib/Singleton/DialogSystem.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
@@ -15,6 +15,9 @@ namespace Main.Lib.Singleton
         [SerializeField] private Button dialogButtonTemplate;
         [SerializeField] private TMP_Text dialogText;
         [SerializeField] private TMP_Text senderText;
+        [SerializeField] private TMP_Text continueText;
+
+        private bool _isShowing = false;
 
         protected override void Awake()
         {
@@ -24,21 +27,57 @@ namespace Main.Lib.Singleton
             dialogButtonsContainer.SetActive(false);
             dialogText.text = "";
             senderText.text = "";
+            continueText.gameObject.SetActive(false);
         }
 
         public static void ShowDialog(string message, string sender="")
         {
+            if(Instance._isShowing)
+            {
+                Debug.LogWarning("DialogSystem is already showing");
+                return;
+            }
             _ = Instance._ShowDialog(message, sender);
+        }
+
+        private void Update()
+        {
+            if (!_isShowing)
+                return;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _ = CloseDialog();
+            }
+        }
+
+        private async UniTask CloseDialog()
+        {
+            _isShowing = false;
+            await dialogPanel.transform.DOScale(Vector3.zero, 0.5f).SetUpdate(true).SetLink(Instance.gameObject).AsyncWaitForCompletion();
+            canvas.gameObject.SetActive(false);
+            senderText.text = "";
+            dialogText.text = "";
+            Time.timeScale = 1;
         }
 
         private async UniTask _ShowDialog(string message, string sender = "")
         {
+            Time.timeScale = 0;
             canvas.gameObject.SetActive(true);
             dialogPanel.transform.localScale *= 0;
-            await dialogPanel.transform.DOScale(Vector3.one, 0.5f).SetLink(Instance.gameObject).AsyncWaitForCompletion();
+            await dialogPanel.transform.DOScale(Vector3.one, 0.5f).SetUpdate(true).SetLink(Instance.gameObject).AsyncWaitForCompletion();
+            _isShowing = true;
             senderText.text = sender;
-            dialogText.text = message;
+            var t = "";
+            foreach (var c in message)
+            {
+                dialogText.text = t;
+                t += c;
+                await UniTask.WaitForSeconds(0.05f, ignoreTimeScale:true, cancellationToken:destroyCancellationToken);
+            }
+            continueText.gameObject.SetActive(true);
         }
+
         
         
     }
