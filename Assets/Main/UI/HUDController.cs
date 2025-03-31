0e5e4dcf33ee15888b3601f02bde2b01d557dca7
RIchard Manansala <richard232manansala@gmail.com>
2025-03-31 09:23:18 +0800

    Added Boss Level

--- Assets/Main/UI/HUDController.cs
+++ Assets/Main/UI/HUDController.cs
@@ -1,4 +1,5 @@
 using Main.Lib.Level;
+using Main.Lib.Mobs;
 using Main.Lib.Singleton;
 using Main.Player;
 using UnityEngine;
@@ -11,7 +12,8 @@ namespace Main.UI
         private PlayerController  _player;
         [SerializeField] private AmmoHUDController ammoHUDController = null!;
         [SerializeField] private HealthHUDController healthHUDController = null!;
-
+        [SerializeField] private BossHealthController bossHealthHUDController = null!;
+        
         protected override void Awake()
         {
             base.Awake();
@@ -24,20 +26,29 @@ namespace Main.UI
                 ammoHUDController.Disable();
             if (healthHUDController)
                 healthHUDController.Disable();
+            if(bossHealthHUDController)
+                bossHealthHUDController.Disable();
         }
 
         public void EnterCutsceneMode()
         {
             ammoHUDController.Disable();
             healthHUDController.Disable();
+            bossHealthHUDController.Disable();
         }
 
         public void ExitCutsceneMode()
         {
             ammoHUDController.Enable();
             healthHUDController.Enable();
+            bossHealthHUDController.Enable();
         }
 
+        public void RegisterBoss(EnemyController boss)
+        {
+            bossHealthHUDController.Enable();
+            bossHealthHUDController.Setup(boss);
+        }
 
         public void SetPlayer(PlayerController player)
         {
