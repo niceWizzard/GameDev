0f854dcdbdc4bb879c0845e4bfa7b2ece935fd1e
RIchard Manansala <richard232manansala@gmail.com>
2025-04-01 14:18:46 +0800

    Added states for boss Flee, idle, rest

--- Assets/Main/World/Mobs/Boss/States/IdleState.cs
+++ Assets/Main/World/Mobs/Boss/States/IdleState.cs
@@ -1,9 +1,13 @@
 using Main.Lib.FSM;
 
-namespace Main.World.Mobs.Boss
+namespace Main.World.Mobs.Boss.States
 {
     public class IdleState : State<BossFsm, BossController>
     {
-        
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Agent.Animator.Play("Idle");
+        }
     }
 }
